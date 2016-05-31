a5e6acc622d4f1b159ee021ce90de7a66dcbab26
vijay <vijay@perk.com>
2016-05-31 15:20:46 +0530

    Added listview fragment

---
+++ Fragements/app/src/main/java/weekend/com/fragements/Fragment/ListFragment.java
@@ -0,0 +1,32 @@
+package weekend.com.fragements.Fragment;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.Toast;
+
+import weekend.com.fragements.R;
+
+public class ListFragment extends android.support.v4.app.ListFragment implements AdapterView.OnItemClickListener{
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.activity_list_fragment, container, false);
+        return view;
+    }
+
+    @Override
+    public void onActivityCreated(Bundle savedInstanceState) {
+        super.onActivityCreated(savedInstanceState);
+        ArrayAdapter adapter = ArrayAdapter.createFromResource(getActivity(), R.array.Planets, android.R.layout.simple_list_item_1);
+        setListAdapter(adapter);
+        getListView().setOnItemClickListener(this);
+    }
+
+    @Override
+    public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+        Toast.makeText(getActivity(), "Item: " + position, Toast.LENGTH_SHORT).show();
+    }
+}
